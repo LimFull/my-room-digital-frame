1e2ffb775c44912f30f4ec340115b67b6e58fd34
LimFull <full7002@naver.com>
2024-02-24 01:08:08 +0900

    fix: 이미지 안뜨는 현상

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,17 @@
   const prefix =
     process.env.NODE_ENV === 'production' ? 'https://limfull.github.io/my-room-digital-frame/' : ''
 
+const basPath =
+  process.env.NODE_ENV === 'production' ? '/my-room-digital-frame' : ''
+
 const nextConfig = {
   output: 'export',
   assetPrefix: prefix,
+  images: {
+    loader: 'imgix',
+    path: 'https://limfull.github.io/my-room-digital-frame/',
+  },
+  basePath: basPath,
 }
 
 export default nextConfig;
