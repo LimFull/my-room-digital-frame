898f81643d43853d3c665fdec589d4c11921c396
LimFull <full7002@naver.com>
2024-02-23 23:39:25 +0900

    deploy setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+
+  const prefix =
+    process.env.NODE_ENV === 'production' ? 'https://limfull.github.io/my-room-digital-frame/' : ''
+
+const nextConfig = {
+  output: 'export',
+  assetPrefix: prefix,
+}
 
 export default nextConfig;
